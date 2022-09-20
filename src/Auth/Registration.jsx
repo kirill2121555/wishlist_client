b5e5473927f5dac23408715b17e3ef33095698de
kirill2121555 <kirill2121555@gmail.com>
2022-09-20 15:46:20 +0300

    disable email confirmation

--- src/Auth/Registration.jsx
+++ src/Auth/Registration.jsx
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
 import { registration } from '../http/userApi'
 import { observer } from "mobx-react-lite";
+import { useNavigate } from "react-router-dom";
 
 const Registration = observer(() => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [nick, setNick] = useState('')
   const [date, setDate] = useState(new Date())
+  const navigate = useNavigate();
 
   const click = async () => {
     try {
       let data;
       data = await registration(email, password, nick, date);
-      console.log(data)
-      alert('Rent to the mail and confirm the account')
-      window.location.reload()
+      alert('Log in to your account')
+      navigate("/");
     } catch (e) {
       console.log('error')
     }
@@ -53,20 +54,7 @@ const Registration = observer(() => {
             ></input>
           </div>
         </form>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={click}>Register</button>
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Confirm mail</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                Rent to the mail and confirm the account
-              </div>
-            </div>
-          </div>
-        </div>
+        <button type="button" class="btn btn-primary" onClick={click}>Register</button>
       </div>
     </div>
   </div>
